4361d311b1e18e94c645fd901afc624f7b5a9975
SIDs1de <igorekdenisenkov@yandex.ru>
2022-12-12 19:20:48 +0300

    сверстал первую сырую версию сайта, накидал адаптив

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,55 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+  const burger = () => {
+    const burgerBtn = document.querySelector('.burger');
+    const headerMenu = document.querySelector('.header__menu');
+    const body = document.querySelector('.page');
+
+    burgerBtn.addEventListener('click', () => {
+      if (burgerBtn.classList.contains('_open')) {
+        burgerBtn.classList.replace('_open', '_close')
+      } else if (burgerBtn.classList.contains('_close')) {
+        burgerBtn.classList.replace('_close', '_open')
+      } else {
+        burgerBtn.classList.add('_open')
+      }
+
+      headerMenu.classList.toggle('_open')
+      body.classList.toggle('_fixed')
+    })
+  }
+
+  const scrollLinks = () => {
+    const burgerBtn = document.querySelector('.burger');
+    const headerMenu = document.querySelector('.header__menu');
+    const body = document.querySelector('.page');
+
+    document.querySelectorAll('a[href^="#"').forEach(link => {
+      link.addEventListener('click', function (e) {
+        e.preventDefault()
+
+        headerMenu.classList.remove('_open')
+        body.classList.remove('_fixed')
+        if (burgerBtn.classList.contains('_open')) {
+          burgerBtn.classList.replace('_open', '_close')
+        }
+
+        const href = this.getAttribute('href').substring(1)
+
+        const scrollTarget = document.getElementById(href)
+
+        const topOffset = 70
+        const elementPosition = scrollTarget.getBoundingClientRect().top
+        const offsetPosition = elementPosition - topOffset;
+
+        window.scrollBy({
+          top: offsetPosition,
+          behavior: 'smooth'
+        })
+      })
+    })
+  }
+
+  burger()
+  scrollLinks()
 })
